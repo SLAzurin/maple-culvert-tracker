d8d5fced073d8848ded010085e299f35184538c2
SLAzurin <69220163+SLAzurin@users.noreply.github.com>
2023-04-13 08:20:10 +0000

    Add chartlabels

--- chartmaker/index.ts
+++ chartmaker/index.ts
@@ -1,7 +1,11 @@
 import fs from "fs"
 import { ChartJSNodeCanvas } from "chartjs-node-canvas"
 import { ChartConfiguration } from "chart.js"
+import Chart from "chart.js/auto"
+import ChartDataLabels from "chartjs-plugin-datalabels"
 import sharp from "sharp"
+
+Chart.register(ChartDataLabels)
 
 const data: { label: string; score: number }[] = JSON.parse(
   process.env.DATA
@@ -26,13 +30,18 @@ data.forEach(row => {
 })
 
 const lineChartConfig: ChartConfiguration<any> = {
+  plugins: [ChartDataLabels],
   type: "line",
   data: {
     labels: labels,
     datasets: [
       {
-        label: "Score by week",
+        label: "Culvert score by week",
         data: rawData,
+        datalabels: {
+          align: "end",
+          anchor: "end",
+        },
         fill: false,
         borderColor: "rgba(54, 162, 235, 1)",
         tension: 0.1,
@@ -40,7 +49,22 @@ const lineChartConfig: ChartConfiguration<any> = {
       },
     ],
   },
-  options: {},
+  options: {
+    plugins: {
+      datalabels: {
+        backgroundColor: function () {
+          return "rgba(54, 162, 235, 1)"
+        },
+        borderRadius: 4,
+        color: "white",
+        font: {
+          weight: "bold",
+        },
+        formatter: Math.round,
+        padding: 6,
+      },
+    },
+  },
 }
 
 sharp("bg.png")
