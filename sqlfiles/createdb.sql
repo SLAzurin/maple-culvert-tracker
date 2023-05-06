6a5c7307011e17978586f787cf09ff0af96dc69a
SLAzurin <69220163+SLAzurin@users.noreply.github.com>
2023-05-06 00:36:37 +0000

    Simplify db

--- sqlfiles/createdb.sql
+++ sqlfiles/createdb.sql
@@ -1,33 +1,13 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
-CREATE TABLE IF NOT EXISTS discord_servers (
-  id SERIAL PRIMARY KEY,
-  guild_name varchar(255) not null,
-  discord_server_native_id varchar(255) not null unique
-);
-
 -- many characters allowed per discord user
-CREATE TABLE IF NOT EXISTS guild_characters (
-  id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS characters (
+  id BIGSERIAL PRIMARY KEY,
   maple_character_name varchar(255) not null unique,
-  discord_server_id int not null,
-  discord_user_id varchar(255) not null,
-  FOREIGN KEY (discord_server_id) references discord_servers(id) on delete cascade
+  discord_user_id varchar(255) not null
 );
-CREATE INDEX "guild_characters_discord_user_id_key" on guild_characters(discord_user_id);
-
 CREATE TABLE IF NOT EXISTS character_culvert_scores (
-    culvert_date DATE not null,
-    maple_character_name varchar(255) not null,
-    score int not null,
-    PRIMARY KEY (maple_character_name, culvert_date)
+  id BIGSERIAL PRIMARY KEY,
+  culvert_date DATE not null,
+  character_id bigint not null,
+  score int not null,
+  FOREIGN KEY (character_id) references characters(id) on delete cascade
 );
-
--- auth: One time login with command on discord
-CREATE TABLE IF NOT EXISTS otp_codes (
-  id UUID  PRIMARY KEY DEFAULT uuid_generate_v4(),
-  discord_server_id varchar(255) not null,
-  discord_user_id varchar(255) not null,
-  expires timestamptz DEFAULT (now() + interval '2 hours')
-);
-CREATE INDEX "otp_codes_discord_user_id_key" on otp_codes(discord_user_id);
