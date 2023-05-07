a23c9cb11560fa39989d197f59755fec25ade671
SLAzurin <69220163+SLAzurin@users.noreply.github.com>
2023-05-07 21:56:41 +0000

    add proxy

--- culvert-web/vite.config.ts
+++ culvert-web/vite.config.ts
@@ -6,6 +6,13 @@ export default defineConfig({
   plugins: [react()],
   server: {
     open: true,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
   build: {
     outDir: "build",
