6cddcfcbf1cef7d323c0361e3967199c877aec76
SLAzurin <69220163+SLAzurin@users.noreply.github.com>
2023-05-08 07:28:46 +0000

    Add bootstrap to culvert-web

--- culvert-web/src/main.tsx
+++ culvert-web/src/main.tsx
@@ -5,6 +5,11 @@ import { store } from "./app/store"
 import App from "./App"
 import "./index.css"
 
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css"
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min"
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
