f252d61e1a7d86a2fd129f4412eed806ccc06bb6
SLAzurin <69220163+SLAzurin@users.noreply.github.com>
2024-04-28 15:45:30 -0400

    Add router support for main.tsx

--- culvert-web/src/main.tsx
+++ culvert-web/src/main.tsx
@@ -5,17 +5,35 @@ import { persistor, store } from "./app/store"
 import App from "./App"
 import "./index.css"
 import { PersistGate } from "redux-persist/integration/react"
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom"
 
 // Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css"
 // Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min"
+import Rename from "./routes/rename"
+import NewChar from "./routes/newchar"
+import LinkDiscord from "./routes/LinkDiscord"
+
+const router = createBrowserRouter(
+  createRoutesFromElements([
+    <Route path="/" element={<App />} />,
+    <Route path="/rename" element={<Rename />} />,
+    <Route path="/newchar" element={<NewChar />} />,
+    <Route path="/linkdiscord" element={<LinkDiscord />} />,
+  ]),
+)
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <App />
+        <RouterProvider router={router} />
       </PersistGate>
     </Provider>
   </React.StrictMode>,
