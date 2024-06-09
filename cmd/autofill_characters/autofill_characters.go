58746f9ec509946b25dce6d90d4728bb956e6540
SLAzurin <69220163+SLAzurin@users.noreply.github.com>
2024-06-08 23:11:03 -0400

    feat: add autofill cmd entrypoint script

---
+++ cmd/autofill_characters/autofill_characters.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/slazurin/maple-culvert-tracker/internal/api/helpers"
+	"github.com/slazurin/maple-culvert-tracker/internal/apiredis"
+	"github.com/slazurin/maple-culvert-tracker/internal/data"
+	"github.com/slazurin/maple-culvert-tracker/internal/db"
+)
+
+/*
+main parses the discord_nickname from Valkey and automatically INSERT them to the PostgresDB with pattern: "preferred_name - char_name1/char_name2"
+
+It is not recommended to run this on a repeat schedule because it cannot handle name changes, and will easily end up with duplicate names.
+*/
+func main() {
+	val, err := apiredis.RedisDB.Get(context.Background(), "discord_members_"+os.Getenv("DISCORD_GUILD_ID")).Result()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	discordMembers := []data.WebGuildMember{}
+
+	err = json.Unmarshal([]byte(val), &discordMembers)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println("[DB] BEGIN TX")
+	tx, err := db.DB.Begin()
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+
+	for _, m := range discordMembers {
+		if !strings.Contains(m.DiscordNickname, " - ") {
+			log.Println("[WARN] Skipping user", m, "because of missing dash separator")
+			continue
+		}
+		// The line below is responsible for pattern matching with the discord server nickname
+		log.Println("Processing", m.DiscordNickname)
+		chars := strings.Split(strings.Split(m.DiscordNickname, " - ")[1], "/")
+		for _, char := range chars {
+			trimmedCharName := strings.Trim(char, " ")
+			trimmedCharName = strings.Trim(trimmedCharName, ".")
+			trimmedCharName = strings.Trim(trimmedCharName, " ⭐")
+			log.Println("Onto", trimmedCharName)
+			time.Sleep(time.Second)
+			charData, err := helpers.FetchCharacterData(trimmedCharName, os.Getenv("MAPLE_REGION"))
+
+			if err != nil {
+				log.Println("[WARN]", trimmedCharName, "not found in official rankings and will be skipped")
+				continue
+			}
+			log.Println(charData.CharacterName, "will be inserted")
+
+			// insert with conflict handling
+			rows, err := db.DB.Query("SELECT maple_character_name from characters where maple_character_name = $1", charData.CharacterName)
+
+			if err != nil {
+				log.Fatalln(err)
+				return
+			}
+
+			if rows.Next() {
+				rows.Close()
+				log.Println("[WARN] Duplicate character name found in DB, skipping.")
+				continue
+			}
+			rows.Close()
+
+			// DB guaranteed safe to insert
+			log.Println("INSERT INTO characters (maple_character_name, discord_user_id) VALUES ($1, $2)", charData.CharacterName, m.DiscordUserID)
+			_, err = tx.Exec("INSERT INTO characters (maple_character_name, discord_user_id) VALUES ($1, $2)", charData.CharacterName, m.DiscordUserID)
+			if err != nil {
+				log.Fatalln(err)
+				return
+			}
+		}
+	}
+
+	// Add switch for dry run
+	if os.Getenv("DRY_RUN") != "" {
+		log.Println("[WARN] Dry run enabled, no queries will be executed")
+		log.Println("[DB] ROLLBACK")
+		log.Println(tx.Rollback())
+		return
+	}
+	log.Println("[DB] COMMIT")
+	log.Println(tx.Commit())
+}
