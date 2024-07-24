22e2f99013c8ad7eee6d9d5575efaf3d2552e312
SLAzurin <69220163+SLAzurin@users.noreply.github.com>
2024-07-23 20:41:28 -0400

    feat: add new output style for culvert cmd

--- internal/data/playerranking.go
+++ internal/data/playerranking.go
@@ -4,6 +4,7 @@ type PlayerRank struct {
 	CharacterName   string `json:"characterName"`
 	CharacterImgURL string `json:"characterImgURL"`
 	JobName         string `json:"jobName"`
+	Level           int    `json:"level"`
 }
 
 type PlayerRankingResponse struct {
