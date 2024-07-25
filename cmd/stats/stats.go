352625fee95fe54fa3962db0b9c01dbcfeaec013
SLAzurin <69220163+SLAzurin@users.noreply.github.com>
2024-07-25 17:53:52 -0400

    feat: use jet to query stats

--- cmd/stats/stats.go
+++ cmd/stats/stats.go
@@ -1,29 +1,37 @@
 package main
 
+//lint:file-ignore ST1001 Dot imports by jet
+
 import (
 	"log"
 	"time"
+
+	. "github.com/go-jet/jet/v2/postgres"
+	"github.com/slazurin/maple-culvert-tracker/.gen/mapleculverttrackerdb/public/model"
+	. "github.com/slazurin/maple-culvert-tracker/.gen/mapleculverttrackerdb/public/table"
 
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/slazurin/maple-culvert-tracker/internal/db"
 )
 
 func main() {
-	now := time.Now()
+	now := time.Now().UTC()
+	now = now.Add(time.Duration(now.Hour()) * time.Hour * -1)
+	now = now.Truncate(time.Hour)
 	for now.Weekday() != time.Sunday {
 		now = now.Add(time.Hour * -24)
 	}
 	last12WeeksCulvertRaw := []time.Time{}
-	last12WeeksCulvertDate := []string{}
+	// last12WeeksCulvertDate := []string{}
 	for i := 0; i < 12; i++ {
 		last12WeeksCulvertRaw = append(last12WeeksCulvertRaw, now)
 		now = now.Add(time.Hour * -24 * 7)
 	}
-	for _, v := range last12WeeksCulvertRaw {
-		last12WeeksCulvertDate = append(last12WeeksCulvertDate, v.Format("2006-01-02"))
-	}
+	// for _, v := range last12WeeksCulvertRaw {
+	// 	last12WeeksCulvertDate = append(last12WeeksCulvertDate, v.Format("2006-01-02"))
+	// }
 
-	log.Println(last12WeeksCulvertDate)
+	// log.Println(last12WeeksCulvertDate)
 
 	// Fetch active characters
 	stmt, _ := db.DB.Prepare(`select character_culvert_scores.character_id as id, maple_character_name
@@ -31,7 +39,7 @@ func main() {
 	inner join characters on characters.id = character_culvert_scores.character_id
     where culvert_date = $1`)
 
-	rows, _ := stmt.Query(last12WeeksCulvertDate[0])
+	rows, _ := stmt.Query(last12WeeksCulvertRaw[0].Format("2006-01-02"))
 
 	chars := []struct {
 		Name string
@@ -50,24 +58,40 @@ func main() {
 	stmt.Close()
 
 	for _, v := range chars {
-		stmt, _ := db.DB.Prepare(`select culvert_date, score from character_culvert_scores where character_id = $1 order by culvert_date desc`)
-		rows, _ := stmt.Query(v.ID)
+		// stmt, _ := db.DB.Prepare(`select culvert_date, score from character_culvert_scores where character_id = $1 order by culvert_date desc`)
+		// rows, _ := stmt.Query(v.ID)
 
-		scores := []struct {
-			Date  string
-			Score int
-		}{}
-		for rows.Next() {
-			v := struct {
-				Date  string
-				Score int
-			}{}
-			rows.Scan(&v.Date, &v.Score)
-			scores = append(scores, v)
-		}
-		log.Println(v.Name, scores)
-		rows.Close()
-		stmt.Close()
+		// scores := []struct {
+		// 	Date  string
+		// 	Score int
+		// }{}
+		// for rows.Next() {
+		// 	v := struct {
+		// 		Date  string
+		// 		Score int
+		// 	}{}
+		// 	rows.Scan(&v.Date, &v.Score)
+		// 	scores = append(scores, v)
+		// }
+		// log.Println(v.Name, scores)
+		// rows.Close()
+		// stmt.Close()
+
+		stmt := SELECT(
+			CharacterCulvertScores.CulvertDate,
+			CharacterCulvertScores.Score).
+			FROM(CharacterCulvertScores).
+			WHERE(
+				CharacterCulvertScores.CharacterID.EQ(Int64(v.ID)),
+			).
+			ORDER_BY(
+				CharacterCulvertScores.CulvertDate.DESC(),
+			)
+
+		dest := []model.CharacterCulvertScores{}
+		stmt.Query(db.DB, &dest)
+
+		log.Println(dest)
 	}
 
 	// sandbaggers for the last 12 weeks
--- go.mod
+++ go.mod
@@ -5,13 +5,12 @@ go 1.21
 require (
 	github.com/bwmarrin/discordgo v0.28.1
 	github.com/gin-gonic/gin v1.10.0
+	github.com/go-jet/jet/v2 v2.11.1
 	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
 	github.com/redis/go-redis/v9 v9.6.1
 )
-
-require github.com/google/go-cmp v0.6.0 // indirect
 
 require (
 	github.com/bytedance/sonic v1.11.9 // indirect
@@ -19,14 +18,15 @@ require (
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/gabriel-vasile/mimetype v1.4.4 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/go-jet/jet/v2 v2.11.1 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.22.0 // indirect
 	github.com/goccy/go-json v0.10.3 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.8 // indirect
@@ -35,6 +35,8 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	golang.org/x/arch v0.8.0 // indirect
