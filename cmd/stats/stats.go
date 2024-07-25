5886504248220c3130dd2a9b12edb73b34bb94e4
SLAzurin <69220163+SLAzurin@users.noreply.github.com>
2024-07-25 18:13:01 -0400

    feat: fully use jet for stats

--- cmd/stats/stats.go
+++ cmd/stats/stats.go
@@ -7,7 +7,6 @@ import (
 	"time"
 
 	. "github.com/go-jet/jet/v2/postgres"
-	"github.com/slazurin/maple-culvert-tracker/.gen/mapleculverttrackerdb/public/model"
 	. "github.com/slazurin/maple-culvert-tracker/.gen/mapleculverttrackerdb/public/table"
 
 	_ "github.com/joho/godotenv/autoload"
@@ -15,80 +14,51 @@ import (
 )
 
 func main() {
-	now := time.Now().UTC()
-	now = now.Add(time.Duration(now.Hour()) * time.Hour * -1)
-	now = now.Truncate(time.Hour)
-	for now.Weekday() != time.Sunday {
-		now = now.Add(time.Hour * -24)
-	}
+	stmt := SELECT(MAX(CharacterCulvertScores.CulvertDate).AS("culvert_date")).FROM(CharacterCulvertScores)
+	dest := struct {
+		CulvertDate time.Time
+	}{}
+	stmt.Query(db.DB, &dest)
+	sunday := dest.CulvertDate
+
 	last12WeeksCulvertRaw := []time.Time{}
-	// last12WeeksCulvertDate := []string{}
 	for i := 0; i < 12; i++ {
-		last12WeeksCulvertRaw = append(last12WeeksCulvertRaw, now)
-		now = now.Add(time.Hour * -24 * 7)
+		last12WeeksCulvertRaw = append(last12WeeksCulvertRaw, sunday)
+		sunday = sunday.Add(time.Hour * -24 * 7)
 	}
-	// for _, v := range last12WeeksCulvertRaw {
-	// 	last12WeeksCulvertDate = append(last12WeeksCulvertDate, v.Format("2006-01-02"))
-	// }
 
-	// log.Println(last12WeeksCulvertDate)
-
-	// Fetch active characters
-	stmt, _ := db.DB.Prepare(`select character_culvert_scores.character_id as id, maple_character_name
-    from character_culvert_scores
-	inner join characters on characters.id = character_culvert_scores.character_id
-    where culvert_date = $1`)
-
-	rows, _ := stmt.Query(last12WeeksCulvertRaw[0].Format("2006-01-02"))
+	stmt = SELECT(CharacterCulvertScores.CharacterID.AS("character_id"), Characters.MapleCharacterName.AS("maple_character_name")).FROM(
+		CharacterCulvertScores.INNER_JOIN(Characters, Characters.ID.EQ(CharacterCulvertScores.CharacterID)),
+	).WHERE(CharacterCulvertScores.CulvertDate.EQ(DateT(last12WeeksCulvertRaw[0])))
 
 	chars := []struct {
-		Name string
-		ID   int64
+		CharacterID        int64
+		MapleCharacterName string
 	}{}
 
-	for rows.Next() {
-		v := struct {
-			Name string
-			ID   int64
-		}{}
-		rows.Scan(&v.ID, &v.Name)
-		chars = append(chars, v)
-	}
-	rows.Close()
-	stmt.Close()
+	stmt.Query(db.DB, &chars)
 
 	for _, v := range chars {
-		// stmt, _ := db.DB.Prepare(`select culvert_date, score from character_culvert_scores where character_id = $1 order by culvert_date desc`)
-		// rows, _ := stmt.Query(v.ID)
-
-		// scores := []struct {
-		// 	Date  string
-		// 	Score int
-		// }{}
-		// for rows.Next() {
-		// 	v := struct {
-		// 		Date  string
-		// 		Score int
-		// 	}{}
-		// 	rows.Scan(&v.Date, &v.Score)
-		// 	scores = append(scores, v)
-		// }
-		// log.Println(v.Name, scores)
-		// rows.Close()
-		// stmt.Close()
+		inClauseDates := []Expression{}
+		for _, date := range last12WeeksCulvertRaw {
+			inClauseDates = append(inClauseDates, DateT(date))
+		}
 
 		stmt := SELECT(
-			CharacterCulvertScores.CulvertDate,
-			CharacterCulvertScores.Score).
+			CharacterCulvertScores.CulvertDate.AS("culvert_date"),
+			CharacterCulvertScores.Score.AS("score")).
 			FROM(CharacterCulvertScores).
 			WHERE(
-				CharacterCulvertScores.CharacterID.EQ(Int64(v.ID)),
+				CharacterCulvertScores.CharacterID.EQ(Int64(v.CharacterID)).AND(CharacterCulvertScores.CulvertDate.IN(inClauseDates...)),
 			).
 			ORDER_BY(
 				CharacterCulvertScores.CulvertDate.DESC(),
 			)
 
-		dest := []model.CharacterCulvertScores{}
+		dest := []struct {
+			CulvertDate time.Time
+			Score       int32
+		}{}
 		stmt.Query(db.DB, &dest)
 
 		log.Println(dest)
