65ff7e9b2700f323f01098a1427f673bf043d43e
SLAzurin <69220163+SLAzurin@users.noreply.github.com>
2024-08-05 02:19:55 -0400

    Implement auto_backup

--- cmd/auto_backup/auto_backup.go
+++ cmd/auto_backup/auto_backup.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"log"
 	"os"
+	"os/exec"
+	"strings"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 	_ "github.com/joho/godotenv/autoload"
@@ -18,13 +22,33 @@ func init() {
 	}
 }
 
+func startBackup(s *discordgo.Session, stopChan chan struct{}) {
+	// cmd := exec.Command("/usr/local/bin/pg_dump", "-h", "db16", "-U", os.Getenv("POSTGRES_USER"), "-d", os.Getenv("POSTGRES_DB"), "-p", "5432") // Only use this line outside of docker container
+	cmd := exec.Command("/usr/local/bin/pg_dump", "-h", os.Getenv("CLIENT_POSTGRES_HOST"), "-U", os.Getenv("POSTGRES_USER"), "-d", os.Getenv("POSTGRES_DB"), "-p", os.Getenv("CLIENT_POSTGRES_PORT"))
+	cmd.Env = append(cmd.Env, "PGPASSWORD="+os.Getenv("POSTGRES_PASSWORD"))
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	if err != nil {
+		log.Println(stdout.String())
+		log.Println(stderr.String())
+		panic(err)
+	}
+
+	s.ChannelMessageSendComplex(os.Getenv("DISCORD_REMINDER_CHANNEL_ID"), &discordgo.MessageSend{
+		Content: "Automatic Database backup " + time.Now().Format("2006-01-02"),
+		Files:   []*discordgo.File{{Name: "dump_" + time.Now().Format("2006-01-02") + ".sql", Reader: strings.NewReader(stdout.String())}},
+	})
+	stopChan <- struct{}{}
+}
+
 func main() {
 	stop := make(chan struct{}, 1)
 	s.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
 		log.Printf("Logged in as: %v#%v", s.State.User.Username, s.State.User.Discriminator)
-		s.ChannelMessageSend(os.Getenv("DISCORD_REMINDER_CHANNEL_ID"), "Starting backup")
-		// TODO: backup
-		stop <- struct{}{}
+		go startBackup(s, stop)
 	})
 	err := s.Open()
 	if err != nil {
