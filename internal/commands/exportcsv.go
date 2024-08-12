498c5d83afcd8fb477ff92082cf3eda57f421511
SLAzurin <69220163+SLAzurin@users.noreply.github.com>
2024-08-12 03:07:28 -0400

    feat: wip export csv logic

--- internal/commands/exportcsv.go
+++ internal/commands/exportcsv.go
@@ -1,16 +1,28 @@
 package commands
 
+//lint:file-ignore ST1001 Dot imports by jet
+
 import (
+	"bytes"
+	"encoding/csv"
+	"io"
+	"log"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/bwmarrin/discordgo"
+	. "github.com/go-jet/jet/v2/postgres"
+	. "github.com/slazurin/maple-culvert-tracker/.gen/mapleculverttrackerdb/public/table"
+	"github.com/slazurin/maple-culvert-tracker/internal/apiredis"
+	"github.com/slazurin/maple-culvert-tracker/internal/commands/helpers"
+	"github.com/slazurin/maple-culvert-tracker/internal/db"
 )
 
 func exportcsv(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	// Parse discord param character-name
 	date := ""
-	weeks := int64(8)
+	weeks := int64(4)
 	options := i.ApplicationCommandData().Options
 	for _, v := range options {
 		if v.Name == "date" {
@@ -20,10 +32,21 @@ func exportcsv(s *discordgo.Session, i *discordgo.InteractionCreate) {
 			weeks = v.IntValue()
 		}
 	}
-
+	rawDate, err := helpers.GetLatestSundayDate(db.DB)
+	if err != nil {
+		log.Println("exportcsv", err)
+		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: "Failed to retrieve latest sunday date. See server logs.",
+				Flags:   discordgo.MessageFlagsEphemeral,
+			},
+		})
+		return
+	}
 	// Validate date format
 	if date != "" {
-		_, err := time.Parse("2006-01-02", date) // YYYY-MM-DD
+		rawDate, err = time.Parse("2006-01-02", date) // YYYY-MM-DD
 		if err != nil {
 			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 				Type: discordgo.InteractionResponseChannelMessageWithSource,
@@ -34,13 +57,129 @@ func exportcsv(s *discordgo.Session, i *discordgo.InteractionCreate) {
 			})
 			return
 		}
+		for rawDate.Weekday() != time.Sunday {
+			rawDate = rawDate.Add(time.Hour * -24)
+		}
 	}
+	originalInputDate := rawDate.Format("2006-01-02")
+
+	allDates := []Expression{}
+	allDatesRaw := []time.Time{}
+	for i := 0; i < int(weeks); i++ {
+		allDates = append(allDates, DateT(rawDate))
+		allDatesRaw = append(allDatesRaw, rawDate)
+		rawDate = rawDate.Add(time.Hour * 24 * -7)
+	}
+
+	chars, err := helpers.GetAcviveCharacters(apiredis.RedisDB, db.DB)
+	if err != nil {
+		log.Println("export csv get active chars failed", err)
+		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: "Export csv command failed, could not get active characters",
+			},
+		})
+		return
+	}
+
+	charIDsInExpr := []Expression{}
+	for _, v := range *chars {
+		charIDsInExpr = append(charIDsInExpr, Int64(v.ID))
+	}
+
+	/*
+		SELECT * data for x weeks and x date
+		for each dataset, create map[string]map[string]int representing map[date]map[character_name]score
+		create new csv from the map above
+	*/
+
+	stmt := SELECT(Characters.MapleCharacterName.AS("name"), CharacterCulvertScores.CulvertDate.AS("culvert_date"), CharacterCulvertScores.Score.AS("score")).FROM(CharacterCulvertScores.INNER_JOIN(Characters, Characters.ID.EQ(CharacterCulvertScores.CharacterID))).WHERE(CharacterCulvertScores.CulvertDate.IN(allDates...).AND(CharacterCulvertScores.CharacterID.IN(charIDsInExpr...))).ORDER_BY(Characters.MapleCharacterName.ASC())
+
+	dest := []struct {
+		CulvertDate time.Time
+		Score       int32
+		Name        string
+	}{}
+	err = stmt.Query(db.DB, &dest)
+	if err != nil {
+		log.Println("export csv select stmt failed", err)
+		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: "Export csv failed getting culvert data from db. See server logs.",
+			},
+		})
+		return
+	}
+
+	m := map[string]map[string]int{}
+
+	for _, v := range dest {
+		d := v.CulvertDate.Format("2006-01-02")
+		if _, ok := m[d]; !ok {
+			m[d] = map[string]int{}
+		}
+		m[d][v.Name] = int(v.Score)
+	}
+
+	bytesBuffer := bytes.NewBufferString("")
+	w := csv.NewWriter(bytesBuffer)
+	defer w.Flush()
+
+	charNamesHeader := []string{"dates"}
+	for _, v := range *chars {
+		charNamesHeader = append(charNamesHeader, v.MapleCharacterName)
+	}
+	err = w.Write(charNamesHeader)
+	if err != nil {
+		log.Println("export csv write charNamesHeader", err)
+		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: "Export csv failed to write character names header. See server logs.",
+			},
+		})
+		return
+	}
+
+	culvertData := [][]string{}
+	for _, v := range allDatesRaw {
+		d := v.Format("2006-01-02")
+		row := []string{d}
+		for _, c := range *chars {
+			row = append(row, strconv.Itoa(m[d][c.MapleCharacterName]))
+		}
+		culvertData = append(culvertData, row)
+	}
+
+	for n, row := range culvertData {
+		err = w.Write(row)
+		if err != nil {
+			log.Println("export csv write culvertData at "+strconv.Itoa(n), err)
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: "Export csv failed to write character names header. See server logs.",
+				},
+			})
+			return
+		}
+	}
+
+	contentRaw, _ := io.ReadAll(bytesBuffer)
 
 	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseChannelMessageWithSource,
 		Data: &discordgo.InteractionResponseData{
-			Content: "weeks " + strconv.FormatInt(weeks, 10),
-			Flags:   discordgo.MessageFlagsEphemeral,
+			Content: "Export for " + strconv.Itoa(int(weeks)) + " weeks on " + originalInputDate,
+			Files: []*discordgo.File{
+				{
+					Name:        "export_" + originalInputDate + ".csv",
+					ContentType: "text/csv",
+					Reader:      strings.NewReader(string(contentRaw)),
+				},
+			},
 		},
 	})
 }
