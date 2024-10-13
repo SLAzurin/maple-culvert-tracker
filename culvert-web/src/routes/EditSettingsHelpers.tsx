3f0e18041f323de0df07c11a6d60cd9b51d54a93
SLAzurin <69220163+SLAzurin@users.noreply.github.com>
2024-10-13 04:32:29 -0400

    fix: display current value

--- culvert-web/src/routes/EditSettingsHelpers.tsx
+++ culvert-web/src/routes/EditSettingsHelpers.tsx
@@ -4,19 +4,22 @@ export const getHumanValue = (editableValues: any, key: string) => {
 			if (editableValues[key].multiple) {
 				const cs = editableValues[key].value.split(",");
 				const cn: string[] = [];
-				cs.forEach((c: string) => {
-					cn.push(
-						editableValues[key].available_channels.filter(
-							(c: any) => c.id === c && c.type === 0,
-						).name || c,
-					);
+				cs.forEach((channelID: string) => {
+					const channelName = (
+						editableValues[key].available_channels as any[]
+					).find(
+						(cdata: any) => cdata.id === channelID && cdata.type === 0,
+					)?.name;
+					cn.push(channelName || channelID);
 				});
 				return cn.join(", ");
 			} else
 				return (
-					editableValues[key].available_channels.filter(
-						(c: any) => c.id === editableValues[key].value && c.type === 0,
-					).name || editableValues[key].value
+					editableValues[key].available_channels.find(
+						(channelData: any) =>
+							channelData.id === editableValues[key].value &&
+							channelData.type === 0,
+					)?.name || editableValues[key].value
 				);
 		case "discord_role":
 			if (editableValues[key].multiple) {
@@ -24,22 +27,25 @@ export const getHumanValue = (editableValues: any, key: string) => {
 				const rn: string[] = [];
 				rs.forEach((r: string) => {
 					rn.push(
-						editableValues[key].available_roles.filter((r: any) => r.id === r)
-							.name || r,
+						editableValues[key].available_roles.find(
+							(roleData: any) => roleData.id === r,
+						)?.name || r,
 					);
 				});
 				return rn.join(", ");
 			} else
 				return (
-					editableValues[key].available_roles.filter(
+					editableValues[key].available_roles.find(
 						(r: any) => r.id === editableValues[key].value,
-					).name || editableValues[key].value
+					)?.name || editableValues[key].value
 				);
 		case "string":
 			return editableValues[key].value;
 		case "selection":
-			return editableValues[key].available_selections.filter(
-				(s: string) => s === editableValues[key].value,
+			return (
+				editableValues[key].available_selections.find(
+					(s: string) => s === editableValues[key].value,
+				) ?? "BAD VALUE"
 			);
 	}
 };
