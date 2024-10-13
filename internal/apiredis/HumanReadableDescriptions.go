8147fa26d63cdec8563d4219f2b58a3e5cfbb762
SLAzurin <69220163+SLAzurin@users.noreply.github.com>
2024-10-13 17:27:16 -0400

    docs: update human readable config

--- internal/apiredis/HumanReadableDescriptions.go
+++ internal/apiredis/HumanReadableDescriptions.go
@@ -6,12 +6,12 @@ type HumanReadableDescriptions struct {
 }
 
 var confReadableNameAndDescriptions = map[redisInternalKey]HumanReadableDescriptions{
-	CONF_DISCORD_ADMIN_CHANNEL_ID:            {"Discord Admin Channel ID", "The ID of the channel where the bot will send admin notifications"},
-	CONF_DISCORD_MEMBERS_MAIN_CHANNEL_ID:     {"Discord Members Main Channel ID", "The ID of the channel where the bot will interact with guild members"},
-	CONF_DISCORD_GUILD_ROLE_IDS:              {"Discord Guild Role IDs", "The IDs of guild members. Comma separated"},
+	CONF_DISCORD_ADMIN_CHANNEL_ID:            {"Discord Admin Channel ID", "REQUIRED The ID of the channel where the bot will send admin notifications"},
+	CONF_DISCORD_MEMBERS_MAIN_CHANNEL_ID:     {"Discord Members Main Channel ID", "REQUIRED The ID of the channel where the bot will interact with guild members"},
+	CONF_DISCORD_GUILD_ROLE_IDS:              {"Discord Guild Role IDs", "REQUIRED The IDs of guild members. Comma separated"},
 	OPTIONAL_CONF_DISCORD_REMINDER_SUFFIX:    {"Discord Reminder Suffix", "Optional fluff suffix on the daily reminder message"},
-	OPTIONAL_CONF_MAPLE_REGION:               {"Maple Region", "The region of the server. Must be 'na' or 'eu'"},
-	OPTIONAL_CONF_CULVERT_DUEL_THUMBNAIL_URL: {"Culvert Duel Thumbnail URL", "The URL of the thumbnail of the duel image"},
+	OPTIONAL_CONF_MAPLE_REGION:               {"Maple Region", "Optional region of the server. Must be 'na' or 'eu', empty defaults to 'na'"},
+	OPTIONAL_CONF_CULVERT_DUEL_THUMBNAIL_URL: {"Optional Culvert Duel Thumbnail URL", "The URL of the thumbnail of the duel image"},
 }
 
 func GetHumanReadableDescriptions(k redisInternalKey) *HumanReadableDescriptions {
