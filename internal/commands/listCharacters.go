a5f2ff41efef6dcd583b6d1222815e9ab36bc493
SLAzurin <69220163+SLAzurin@users.noreply.github.com>
2025-10-28 21:06:45 -0400

    fix: listCharacter for only active characters

--- internal/commands/listCharacters.go
+++ internal/commands/listCharacters.go
@@ -1,35 +1,31 @@
 package commands
 
-//lint:file-ignore ST1001 Dot imports by jet
-
 import (
 	"encoding/json"
-	"errors"
 	"log"
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
-	. "github.com/go-jet/jet/v2/postgres"
-	"github.com/go-jet/jet/v2/qrm"
-	. "github.com/slazurin/maple-culvert-tracker/.gen/mapleculverttrackerdb/public/table"
+	"github.com/slazurin/maple-culvert-tracker/internal/apiredis"
+	"github.com/slazurin/maple-culvert-tracker/internal/commands/helpers"
 	"github.com/slazurin/maple-culvert-tracker/internal/db"
 )
 
 func listCharacters(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	characters := []string{}
-	stmt := SELECT(Characters.MapleCharacterName).FROM(Characters).WHERE(Characters.DiscordUserID.NOT_EQ(String("1"))).ORDER_BY(Characters.MapleCharacterName.ASC())
-
-	err := stmt.Query(db.DB, &characters)
-	if err != nil && !errors.Is(err, qrm.ErrNoRows) {
-		log.Println("listCharacters: failed to query characters", err)
+	charactersFullData, err := helpers.GetActiveCharacters(apiredis.RedisDB, db.DB)
+	if err != nil {
+		log.Println("Error fetching active characters:", err)
 		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 			Type: discordgo.InteractionResponseChannelMessageWithSource,
 			Data: &discordgo.InteractionResponseData{
-				Content: "Failed to fetch characters from database!",
-				Flags:   discordgo.MessageFlagsEphemeral,
+				Content: "Sorry, I encountered an error while fetching your characters. Please try again later.",
 			},
 		})
 		return
+	}
+	for _, v := range *charactersFullData {
+		characters = append(characters, v.MapleCharacterName)
 	}
 
 	jsonCharacters, _ := json.Marshal(characters)
