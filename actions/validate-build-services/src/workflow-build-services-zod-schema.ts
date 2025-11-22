a595b76801bfb0b38b680af6147caf0b50278e4c
SLAzurin <69220163+SLAzurin@users.noreply.github.com>
2025-11-21 19:23:29 -0500

    blah

---
+++ actions/validate-build-services/src/workflow-build-services-zod-schema.ts
@@ -0,0 +1,9 @@
+import { object, string, array } from 'zod';
+
+export const buildServicesSchema = array(object({
+    name: string(),
+    dockerfile: string().optional().default("Dockerfile.goapps"),
+    context: string().optional().default("."),
+    target: string(),
+    buildArgs: object({}).catchall(string()).optional(),
+}));
