319bda3910575b34bccbb6cb5fcddf1e09e7769f
SLAzurin <69220163+SLAzurin@users.noreply.github.com>
2026-02-08 21:06:45 -0500

    fix: api healthcheck

--- cmd/http_healthcheck/main.go
+++ cmd/http_healthcheck/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"time"
 )
 
 func main() {
@@ -14,12 +15,23 @@ func main() {
 	if *urlFlag == "" {
 		log.Fatalln("url flag empty")
 	}
-
-	r, err := http.Get(*urlFlag)
-	if err != nil {
-		log.Fatalln(err.Error())
-	}
-	if r.StatusCode != 200 {
-		log.Fatalln("failed with " + strconv.Itoa(r.StatusCode))
+	retry := true
+	for retry {
+		retry = false
+		r, err := http.Get(*urlFlag)
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+		if r.StatusCode == http.StatusServiceUnavailable {
+			retry = true
+			dStr := r.Request.Response.Header.Get("Retry-After")
+			retryAfterD, err := time.Parse(time.RFC1123, dStr)
+			if dStr == "" || err != nil {
+				log.Fatal("failed StatusServiceUnavailable invalid date in Retry-After header")
+			}
+			time.Sleep(time.Until(retryAfterD))
+		} else if r.StatusCode != 200 {
+			log.Fatalln("failed with " + strconv.Itoa(r.StatusCode))
+		}
 	}
 }
--- internal/api/api.go
+++ internal/api/api.go
@@ -1,6 +1,10 @@
 package api
 
 import (
+	"net/http"
+	"sync"
+	"time"
+
 	"github.com/bwmarrin/discordgo"
 	"github.com/gin-gonic/gin"
 	"github.com/slazurin/maple-culvert-tracker/internal/api/controllers"
@@ -8,10 +12,35 @@ import (
 )
 
 var DiscordSession *discordgo.Session
+var startHealthcheckFail *time.Time
+var startHealthcheckFailMutex = sync.Mutex{}
 
 func NewRouter() *gin.Engine {
 	controllers.DiscordSession = DiscordSession
 	router := gin.Default()
+	router.GET("health", func(c *gin.Context) {
+		// no need to check http, this is self validated
+		d := time.Now().UTC()
+		c.Writer.Header().Set("Date", d.Format(time.RFC1123))
+		if DiscordSession == nil || !DiscordSession.DataReady {
+			startHealthcheckFailMutex.Lock()
+			defer startHealthcheckFailMutex.Unlock()
+			if startHealthcheckFail == nil {
+				startHealthcheckFail = &d
+			}
+			status := http.StatusServiceUnavailable
+			if d.After(startHealthcheckFail.Add(30 * time.Second)) {
+				status = http.StatusInternalServerError
+			}
+			if status == http.StatusServiceUnavailable {
+				c.Writer.Header().Set("Retry-After", d.Add(time.Second*5).Format(time.RFC1123))
+			}
+			c.AbortWithStatus(status)
+			return
+		} else if startHealthcheckFail != nil {
+			startHealthcheckFail = nil
+		}
+	})
 	apiGroup := router.Group("/api")
 	{
 		apiGroup.Use(middlewares.AuthMiddleware())
